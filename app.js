66a18310fd4b979c6a9ef3d69d5309b169dd1f97
domparise@gmail.com <domparise@gmail.com>
2014-07-31 04:09:23 -0400

    pushing server-side twitter code

--- app.js
+++ app.js
@@ -20,26 +20,27 @@ function serveIndex (req,res) {
     res.sendfile('public/index.html');
 }
 
+// initialize twitter on start of server, rather than on each /fetch-tweets request
+// authenticating twitter account
+var twit = new require('twitter')({
+    consumer_key: '3xxHwU4Aj3PLFf0SG2C77pfeD',
+    consumer_secret: '5tp9lYTn7KdWNb9GNi1yqrzBYWnwuDeT331NZdUfGohmfHkR6P',
+    access_token_key: '1213699087-BvXlfRn4ijklwKU5S7pdVKsCf58uUr29Glf0r3Q',
+    access_token_secret: 'JO7OZbGA363nJ19EGoNL5jrXqrh0lLIiAsBXDVrMrKMuJ'
+});
+
+app.get('/fetch-tweets', function (req,res) {
+	twit.get('/statuses/user_timeline.json', { count: 5 }, function (tweets) {
+		var arr = tweets.map(function (elt) {
+			return elt.text
+		})
+		res.send(arr)
+	});
+});
+
+
 app.use(express.static(__dirname+'/public'));
 
 app.listen(3000,function(){
 	console.log('KTP web server listening on port 3000!');
-});
-
-
-
-app.get('/fetch-tweets', function (req,res) {
-	// authenticating twitter account
-	var util = require('util'),
-	    twitter = require('twitter');
-	var twit = new twitter({
-	    consumer_key: '3xxHwU4Aj3PLFf0SG2C77pfeD',
-	    consumer_secret: '5tp9lYTn7KdWNb9GNi1yqrzBYWnwuDeT331NZdUfGohmfHkR6P',
-	    access_token_key: '1213699087-BvXlfRn4ijklwKU5S7pdVKsCf58uUr29Glf0r3Q',
-	    access_token_secret: 'JO7OZbGA363nJ19EGoNL5jrXqrh0lLIiAsBXDVrMrKMuJ'
-	});
-
-	// var tweets = twit.get('/statuses/kappathetapi.json', { count: 5 }, func);
-
-	res.send(twit.get('/statuses/kappathetapi.json', { count: 5 }, func));
 });
