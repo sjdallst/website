cfecac870f0c2e02ee1da0dee55758ab3c8ce79b
domparise@gmail.com <domparise@gmail.com>
2014-09-19 21:05:36 -0400

    added pwd to sixtynine

--- sixtyniner.js
+++ sixtyniner.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var pwd = process.argv[2]
 
-app.use('/data',express.directory('data'))
-app.use('/data',express.static('data'))
+app.use(express.directory('data'))
+app.use(function (req, res, next) {
+    req.opts = require('querystring').parse(req.url.substring(req.url.indexOf('?')+1))
+    if (req.opts.pwd != pwd) res.send('incorrect password') 
+    else express.static('data')(req,res,next)
+})
 
 app.listen(6969,function(){
     console.log('KTP web server listening on port 6969!')
