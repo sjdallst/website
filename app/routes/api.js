f4713c2787aa2e32b521ec1079c46969a96850ce
domparise@gmail.com <domparise@gmail.com>
2015-01-27 02:18:45 -0500

    added method api method to change password

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,6 +7,7 @@ module.exports = function (api) {
         else return res.status(401).send('Unauthorized token')
     })
 
+    // authenticating the user to the app
     api.post('/login',function (req, res, next) { // send request body as {account, password}
         Account.findById(req.body.account, function (err, account) {
             if (err) throw err
@@ -19,4 +20,33 @@ module.exports = function (api) {
         })
     })
 
+    // authenticating the user and changing their password
+    api.post('/changePassword',function (req, res, next) { 
+        // send request body as {account, oldPassword, newPassword, confirmPassword}
+        Account.findById(req.body.account, function (err, account) {
+            if (err) throw err
+            else if (!account) return res.send('account not found\n')
+            else if (req.body.newPassword != req.body.confirmPassword) return res.send('passwords dont match\n')
+            else account.generateHash(req.body.newPassword, function (err, hash) {
+                if (err) throw err
+                account.password = hash
+                account.save(function (err) {
+                    if (err) throw err
+                    else return res.send('password changed\n')
+                })
+            })
+        })
+    })
+
 }
+/*
+// testing:
+curl -i -X POST -H 'Content-Type: application/json' -H 'x-access-token:5af9a24515589a73d0fa687e69cbaaa15918f833' -d '{"account":"54c7315aea4ea80000d81dd7","password":"dollabills"}' http://localhost:3000/api/login
+'invalid password'
+curl -i -X POST -H 'Content-Type: application/json' -H 'x-access-token:5af9a24515589a73d0fa687e69cbaaa15918f833' -d '{"account":"54c7315aea4ea80000d81dd7","password":"dollabillz"}' http://localhost:3000/api/login
+'success'
+curl -i -X POST -H 'Content-Type: application/json' -H 'x-access-token:5af9a24515589a73d0fa687e69cbaaa15918f833' -d '{"account":"54c7315aea4ea80000d81dd7","oldPassword":"dollabillz","newPassword":"yolo","confirmPassword":"yolo"}' http://localhost:3000/api/changePassword
+'password changed'
+curl -i -X POST -H 'Content-Type: application/json' -H 'x-access-token:5af9a24515589a73d0fa687e69cbaaa15918f833' -d '{"account":"54c7315aea4ea80000d81dd7","password":"yolo"}' http://localhost:3000/api/login
+'success'
+*/
