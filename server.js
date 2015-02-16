449e857c149d80c2ce09d7c63fa12aa3733e3940
domparise@gmail.com <domparise@gmail.com>
2015-02-15 20:18:27 -0500

    updated constition

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ server.get('/wob', function (req,res) {
     return res.redirect('https://www.facebook.com/events/862475787150706/')
 })
 
+server.get('/constitution', function (req,res) {
+    return res.sendfile('public/Constitution.pdf')
+})
+
 //////// rush application stuff //////////
 
 var formidable = require('formidable'),
