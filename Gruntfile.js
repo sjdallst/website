3f06b8d243107faf7b611bcca60806f722a997c7
owen-yang <yoyoyango123@gmail.com>
2016-02-12 01:44:02 -0500

    Added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        nodemon: {
+            dev: {
+                script: 'index.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    grunt.registerTask('default', ['nodemon']);
+};
